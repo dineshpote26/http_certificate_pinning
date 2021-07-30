f7883c68553b0a0deb22191a209617b42a9bb4e1
dinesh.pote <dineshpote26@gmail.com>
2021-07-30 19:11:23 +0530

    changes

--- lib/certificate_pinning_interceptor.dart
+++ lib/certificate_pinning_interceptor.dart
@@ -4,19 +4,21 @@ import 'package:dio/dio.dart';
 import 'package:http_certificate_pinning/http_certificate_pinning.dart';
 
 class CertificatePinningInterceptor extends Interceptor {
-  final List<String> allowedSHAFingerprints;
+  final List<String>? allowedSHAFingerprints;
   final String basePath;
 
-  CertificatePinningInterceptor({this.allowedSHAFingerprints,this.basePath});
+  CertificatePinningInterceptor(
+      {this.allowedSHAFingerprints, this.basePath = ""});
 
   @override
   Future onRequest(
       RequestOptions options, RequestInterceptorHandler handler) async {
     final secure = await HttpCertificatePinning.check(
-        serverURL: basePath == null || basePath =="" ? options.baseUrl : basePath,
+        serverURL:
+            basePath == "" || basePath.isEmpty ? options.baseUrl : basePath,
         headerHttp: options.headers.map((a, b) => MapEntry(a, b.toString())),
         sha: SHA.SHA256,
-        allowedSHAFingerprints: allowedSHAFingerprints,
+        allowedSHAFingerprints: allowedSHAFingerprints!,
         timeout: 50);
 
     if (secure.contains("CONNECTION_SECURE")) {
